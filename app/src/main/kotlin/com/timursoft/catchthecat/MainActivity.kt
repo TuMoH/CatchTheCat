860f1dc6f994c7bfd5c7cac5dbdcd4657292ac92
TuMoH <tumolllaa@gmail.com>
2016-10-22 01:31:38 +0300

    imports

--- app/src/main/kotlin/com/timursoft/catchthecat/MainActivity.kt
+++ app/src/main/kotlin/com/timursoft/catchthecat/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.timursoft.catchthecat
 
-import android.annotation.TargetApi
+import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.app.Activity
-import android.content.Context
-import android.graphics.Canvas
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.VectorDrawable
-import android.os.Build
-import android.support.v4.content.ContextCompat
 import android.widget.ImageView
 import android.widget.Toast
 import org.jetbrains.anko.*
